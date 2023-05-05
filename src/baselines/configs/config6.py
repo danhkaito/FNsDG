2b96e08af1f0ddc91f1de4e5f61666e461337d32
hyper-windy <69942802+hyper-windy@users.noreply.github.com>
2023-05-05 15:53:13 +0700

    'a'

---
+++ src/baselines/configs/config6.py
@@ -0,0 +1,21 @@
+# Train test one
+MODEL_TYPES = ['mlp', 'lstm']
+
+DATA = ['VLPS']
+
+MODEL_CONF = {
+   'seed': 40,
+   'batch_size': 16,
+   'use_early_stopping': False,
+   'patience': 5,
+   'freeze_pretrain': False,
+   'model_name': 'vinai/phobert-base',
+   'epoch': 10,
+   'learning_rate': 2e-5,
+   'eps': 1e-8,
+   'max_length': 256,
+   'cuda': True,
+   'folder_data': "../../data",
+   'folder_model':"../../saved/baselines",
+   'save_model': False
+}
