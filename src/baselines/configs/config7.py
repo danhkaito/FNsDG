2e288ad7026f744499d35ca57648786660451efc
hyper-windy <69942802+hyper-windy@users.noreply.github.com>
2023-05-11 11:34:04 +0700

    'a'

---
+++ src/baselines/configs/config7.py
@@ -0,0 +1,20 @@
+MODEL_TYPES = ['mlp', 'cnn', 'lstm']
+
+DATA = ['Liar', 'FakeorReal', 'FND']
+
+MODEL_CONF = {
+   'seed': 40,
+   'batch_size': 32,
+   'use_early_stopping': False,
+   'patience': 5,
+   'freeze_pretrain': False,
+   'model_name': 'sentence-transformers/all-distilroberta-v1',
+   'epoch': 10,
+   'learning_rate': 2e-5,
+   'eps': 1e-8,
+   'max_length': 512,
+   'cuda': True,
+   'folder_data': "../../data",
+   'folder_model':"../../saved/baselines",
+   'save_model': False
+}
